ce64447b5815b213ce3910884af03473430b8c9a
Quentin DANJOU <quentin.danjou@live.fr>
2017-08-22 12:58:22 +0800

    Loading correctly a desktop wallpaper on Windows. Issue with the time's datas.

--- DynamicWallpaper.py
+++ DynamicWallpaper.py
@@ -8,9 +8,11 @@ import os
 import datetime
 import urllib.request
 import json
+import random
+import glob
 
 WIN_DRIVE = "C:\\"
-WIN_FOLDER = "Utilisateurs\\quent\\Images\\DynamicWallpaper"
+WIN_FOLDER = "Users\\quent\\Pictures\\DynamicWallpaper"
 UNI_FOLDER = ""
 LOCATION = "Beijing"
 LOCATION_GMT = 6
@@ -44,9 +46,12 @@ def get_previous_sunrise_sunset():
     """
     Use openweathermap API to know the sunrise and sunset time
     """
-    api_service = "api.openweathermap.org/data/2.5/weather?q="
-    request = urllib.request.urlopen(api_service + LOCATION + "&APPID=" + API_KEY).read()
-    data = json.load(request)
+    api_service = "http://api.openweathermap.org/data/2.5/weather?q="
+    request = urllib.request.Request(api_service + LOCATION + "&APPID=" + API_KEY)
+    print("REQUEST DONE!")
+    response = urllib.request.urlopen(request)
+    encoding = response.info().get_content_charset('utf8')
+    data = json.loads(response.read().decode(encoding))
     if data is None or data["cod"] == "404":
         print("Problem occur during sunrise/sunset data acquisition. \
         Your location may haven't been found. \
@@ -71,9 +76,11 @@ def get_available_moment():
     actual_time = datetime.datetime.now()
     actual_time_in_min = actual_time.hour * 60 + actual_time.minute
     sun_data_in_min = get_previous_sunrise_sunset()
-    if actual_time_in_min <= sun_data_in_min[0] + 60 and actual_time_in_min >= sun_data_in_min[1] - 60:
+    if actual_time_in_min <= sun_data_in_min[0] + 60 \
+    and actual_time_in_min >= sun_data_in_min[1] - 60:
         res.append("SUNRISE")
-    elif actual_time_in_min <= sun_data_in_min[1] + 60 and actual_time_in_min >= sun_data_in_min[1] - 60:
+    elif actual_time_in_min <= sun_data_in_min[1] + 60\
+    and actual_time_in_min >= sun_data_in_min[1] - 60:
         res.append("SUNSET")
     if actual_time_in_min <= 12 * 60 and actual_time_in_min > sun_data_in_min[0]:
         res.append("MORNING")
@@ -83,15 +90,46 @@ def get_available_moment():
         res.append("NIGHT")
     else:
         res.append("DAY")
+    print(res)
     return res
 
 def choose_moment(moment_list):
     """
     Randomize moment.
     """
-    return moment_list[0]
+    randomizer = []
+    for moment in moment_list:
+        randomizer.extend([moment for i in range(MOMENTS[moment])])
+    return randomizer[random.randint(0, len(randomizer) - 1)]
 
-def __main__():
-    options = set_os_attribute()
-    moment = get_time_moment()
+def set_wallpaper(sys_options, moment):
+    print(os.path.join(sys_options[1], moment, "*"))
+    all_paths = glob.glob(os.path.join(sys_options[1], moment, "*"))
+    # No file checking for the moment.
+    print(all_paths)
+    if len(all_paths) == 0:
+        return False
+    choosen_imgpath = all_paths[random.randint(0, len(all_paths) - 1)]
+    if sys_options[0] == "windows":
+        print(choosen_imgpath)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, choosen_imgpath, 3)
+        print("wallpaper is set.")
+
+    return True
+
+if __name__ == "__main__":
+    print("STARTING")
+    succeed = False
+    sys_options = set_os_attribute()
+    print("OPTIONS DONE")
+    print(sys_options)
+    loop_protection = 0
+    while succeed is False:
+        loop_protection += 1
+        moment = get_time_moment()
+        print(moment)
+        succeed = set_wallpaper(sys_options, moment)
+        if loop_protection > 20:
+            print("Can't find a wallpaper.", file=sys.stderr)
+            exit(1)
     exit(0)
